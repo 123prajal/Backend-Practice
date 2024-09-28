cd492900f1ac33a6446321c3d8b8112b4426e718
PrajwalKhadka <khadkaprajal1@gmail.com>
2024-09-28 21:39:53 +0545

    user model and video model created also some of the utils file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./src/constants";
 import connectDB from "./db/index.js";
-
+import {app }from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 7000, () => {
+        console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED", err)
+})
